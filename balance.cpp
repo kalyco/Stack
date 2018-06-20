cc2882464b9f7037cf727a5b03fa2e4f6871cfa3
Kayla Comalli <Kayla.comalli@gmail.com>
2018-06-19 20:49:06 -0400

    cleaner. no mem leaks

--- balance.cpp
+++ balance.cpp
@@ -1,61 +1,38 @@
-// char::find
 #include <iostream>
 #include <cctype>
 #include <cstring>
 #include "stack.h"
-#include <map>
 
 using namespace std;
-
-void checkLeftBracks(stack * stk, char s) {
-  char *lstr = new char[3];
-  strcpy(lstr, "([{");
-
-  if (strchr(lstr, s)) {
-    stk->push(s);
-   }
-  delete[] lstr;
-}
-
-char getLeftMatch(int i) {
-  char *lstr = new char[3];
-  strcpy(lstr, "([{");
-  
-  char l = lstr[i];
-  delete[] lstr;
-  return l;
-}
+char lstr [4] = "([{";
 
 bool rightBracksMatch(stack * stk, char s) {
-  char *rstr = new char[3];
-  strcpy(rstr, ")]}");
+  char rstr [4] = ")]}";
   string r = rstr;
 
   if (strchr(rstr, s)) {
     if (stk->isEmpty()) return false;
     size_t idx = r.find(s);
-    char lVal = getLeftMatch(idx);
     char mCheck = stk->top();
-    if (lVal == mCheck) stk->pop();  
+    if (lstr[idx] == mCheck) stk->pop();  
   }
-  delete[] rstr;
   return true;
 }
 
 bool isBalanced(string& s)
 {
+  bool result;
   stack * stk = new stack();
   for (int i=0; i < s.length(); i++) {
-    checkLeftBracks(stk, s[i]);
+    if (strchr(lstr, s[i])) stk->push(s[i]);
     if (!rightBracksMatch(stk, s[i])) {
+      delete stk;
       return false;
     }  
-  }  
-  if (!stk->isEmpty()) {
-    return false;
-  } else {
-    return true;
-  }
+  } 
+  result = stk->isEmpty();
+  delete stk;
+  return result;
 }
 
 int main()
